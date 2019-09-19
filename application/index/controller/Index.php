ffa2bacf9f5197bd358d7b4339491ab44e93c919
YANGQINGYU66 <1501180476@qq.com>
2019-09-19 21:31:35 +0800

    杨飞--个人中心代做

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,19 +9,32 @@ use think\response\Json;
 
 class Index extends Controller
 {
-    //前台展示首页
-    public function index()
+    // 前台首页  获取服务信息   todo  ok
+    public function index(Request $request)
     {
-        $data = goods::all();
-        return $this->fetch('index',['data' => $data]);
+    //  商品名称，价格，已售出数量
+        if($request->isGet()){
+            $goods = Db::table('goods')->all();
+            $num = Db::table('order')->count();
+            $res = json([
+                "error_code"=>0,
+                "message" =>"获取数据成功",
+                "data"=>[
+                    "serviceName"=> $goods[0]['name'],
+                    "servicePrice"=> $goods[0]['price'],
+                    "num"       =>$num,
+                    "favourablePrice"=>"优惠价格",
+                    "serviceImg"=> "服务图片",
+                    "serviceParticularsImg"=> "服务详情图片",
+                    "serviceId"=> "服务ID",
+                    "serviceParticulars"=>"服务详情介绍"
+                ]
+            ]);
+            return $res;
+        }
+
     }
 
-    //商品详情
-    public function goodInfo()
-    {
-        $data = goods::all();
-        return $this->fetch('',['data' => $data]);
-    }
 
     //前台微信登录
     public function wxLogin()
@@ -79,36 +92,91 @@ class Index extends Controller
 
     }
 
-    //个人权益中心
+    // 获取我的权益表     todo
     public function center(Request $request)
     {
-        //两种情况，1.自己的VIP服务， 2.他人的VIP服务 返回的数据区分开
-        $goods = Db::table('goods')->get();
-        $myres = Db::table('center')->wehere('ismy',1)->get();
-        $notmyres = Db::table('cnter')->where('ismy',0)->get();
-        return $this->fetch('center',['myres'=>$myres,'notmyres'=>$notmyres]);
+        if($request->isPost()){
+            $openid = $request->param();
+            $goods = Db::table('goods')->all();
+            //dump($goods[0]);echo '<hr/>';
+            if(!empty($goods)){
+                $res_arr = array();
+                foreach ($goods[0] as $key =>$val) {
+                    $res_arr[$key] = $val;
+                }
+            }else{
+                $res = '没有对应的商品数据';
+            }
+            $openid = $openid?$openid:'1';
+            $order = Db::table('order')->where('openid',$openid)->selectOrFail();
+            if($order>0){
+                $res_order = array();
+                foreach ($order[0] as $key =>$val) {
+                    $res_order[$key]= $val;
+                }
+            }else{
+                $res = '订单信息为空';
+            }
+
+            $type = $request->param();
+//            我的服务
+            if(type == 0){
+
+            }else{                              //为他人购买
+
+            }
+            if(isset($res)){
+                return $res;
+            }else {
+                $return_res = [
+                    "error_code" => 0,
+                    "message" => "获取数据成功",
+                    "data" => [
+                        "isHaveNextPage" => '',
+                        "count"=>'',
+                        "serviceList" => json([
+                            "serviceName" => $res_arr['name'],
+                            "orderId" => $res_order['orderCode'],
+                            "serviceNum" => $res_order['total'],
+                            "usedableNum" => $res_order['total']-$res_order['num'],
+                            "serviceParticulars" => $res_arr['introduce'],
+                            "servicePrice" => $res_arr['price'],
+                            "serviceState" => '',
+                            "serviceParticularsImg" => "",
+                            "serviceImg" => "",
+                            "favourablePrice" => ""
+                        ])
+                    ]
+                ];
+            }
+           return json($return_res['data']);
+        }
     }
 
-    //权益使用接口
+    // 权益使用接口  todo  ok
     public function apply(Request $request)
     {
-        if ($request->isPost()) {
-            $order_id = $request->param('');
-            $ismy = Db::table('center')->where('oeder_id', $order_id)->get('ismy');
+        if ($request->isGet()) {
+            $userId = $request->param();
+            $order_id = 1;
+            $data = Db::table('center')->where('orderCode', $order_id)->field('type')->select();
+            $type =  json($data[0]['type']);
+//            return $ismy;
             //如果个人中心是自己的
-            if ($ismy == 1) {
-                $res = Db::table('center')->where('order_id', $order_id)->update('isuse', 1);
-                if ($res) {
+            if ($type == 0) {
+                $num = Db::table('order')->field(['num','total'])->select();
+                $use_num =  json($num[0]['num']);
+                $total = json($num[0]['total']);
+                if($use_num >= $total){
                     return json([
-                        'code' => 0,
-                        'msg' => '成功',
-                        //跳转到填写身份信息路径
-                        'url' => ''
+                        'code'=>1,
+                        'msg'=>'使用次数已用完'
                     ]);
-                } else {
+                }else{
+                    $res = Db::table('order')->where('userId',$userId)->update('num',$use_num+1);
                     return json([
-                        'code' => 1,
-                        'msg' => '失败'
+                        'code'=>0,
+                        'msg'=>'成功'
                     ]);
                 }
             } else {
@@ -116,100 +184,118 @@ class Index extends Controller
                 //跳转到获取提货码页
                 return json([
                     //提货码路径
+                    'msg' =>'请先获取提货码',
                     'url' => 'yiliao.com/tihuoma'
                 ]);
             }
         }
     }
 
-    //获取提货码
+    // 获取提货码  todo ok
     public function code(Request $request)
     {
-        function createRandomStr1($length){
-            $str = array_merge(range('a','z'),range('A','Z'));
-            shuffle($str);
-            $str = implode('',array_slice($str,0,$length));
-            return $str;
+        if($request->isPost()){
+            function createRandomStr1($length){
+                $str = array_merge(range('a','z'),range('A','Z'));
+                shuffle($str);
+                $str = implode('',array_slice($str,0,$length));
+                return $str;
+            }
+            $first = createRandomStr1(2);
+            function createRandomStr2($length){
+                $str = array_merge(range('0','9'));
+                shuffle($str);
+                $str = implode('',array_slice($str,0,$length));
+                return $str;
+            }
+            $two = createRandomStr2(6);
+            //字符串之间的拼接
+            $code = $first.$two;
+            //todo 存入数据库
+        $data= $request->only(['userId','orderId']);
+        Db::table('order')->insert(['user_id'=>$data['userId'],'orderId'=>$data['orderId'],'code'=>$code]);
+            $return_res = [
+                'error_code'=>0,
+                'msg'=>'获取兑换码成功',
+                'data'=>$data
+            ];
+            return json($return_res);
         }
-        $first = createRandomStr1(2);
-        function createRandomStr2($length){
-            $str = array_merge(range('0','9'));
-            shuffle($str);
-            $str = implode('',array_slice($str,0,$length));
-            return $str;
-        }
-        $two = createRandomStr2(6);
-        //字符串之间的拼接
-        $data = $first.$two;
-        //todo 存入数据库
-        //$userId= $request->param();
-        //$res = Db::table('order')->insert(['user_id'=>$userId,'code'=>$data]);
-        return json([
-            //返回获取提货码当前页
-            'url'=>'yiliao.com/tihuoma',
-            'code'=>0,
-            'msg'=>'获取兑换码成功',
-            'data'=>$data
-        ]);
     }
 
-    //验证前端发来的提货码
+    // todo 兑换服务  todo ok
     public function check(Request $request)
     {
-
-        $code = $request->param();
-        $Code = Db::table('order')->field('code')->find();
-        if($code === $Code){
-            //提货码验证成功   跳转到填写身份信息
-            return redirect('');
-        }else{
-            //提货码验证失败
-            return json([
-                'code'=>1,
-                'msg' =>'您输入的提货码有误',
-                //刷新一下当前页面
-                'url' =>''
-            ]);
+        if($request->isPost()){
+            $code = $request->param('code');
+            $userId = $request->param('userId');
+            $num = Db::table('order')->field(['num','total'])->select();
+            $use_num =  json($num[0]['num']);
+            $total = json($num[0]['total']);
+            if($use_num >= $total){
+                return json([
+                    'code'=>1,
+                    'msg'=>'使用次数已用完'
+                ]);
+            }else{
+                $Code = Db::table('code')->where('userId',$userId)->field('code')->select();
+                if($code === $Code){
+                    //提货码验证成功
+                    $res = Db::table('order')->where('userId',$userId)->update('status',1);
+                    return json([
+                        'code'=>0,
+                        'msg'=>'兑换成功'
+                    ]);
+                }else{
+                    //提货码验证失败
+                    return json([
+                        'code'=>1,
+                        'msg' =>'兑换失败',
+                    ]);
+                }
+            }
         }
     }
 
-    //接收前端发的用户信息
+    //接收前端发的用户信息   todo ok
     public function userInfo(Request $request)
     {
-        $data = $request->only(['name','mobile','cardId']);
-        //验证数据
-        $rule = [
-            'name' => 'require|length:2,20',
-            'mobile' => 'require|length:11,13',
-            'cardId' => 'required|regex:/^[1-6,8][0-9]{5}[1,2,3][0-9]{3}[0,1][0-9][0-3][0-9][0-9]{3}[0-9,X]$/|unique:address'
-        ];
-        $msg = [
-            'name.require' => '用户名为必填项',
-            'name.length' => '用户名的长度应该在2-20之间',
-            'mobile.length' => '联系电话长度有误',
-            'cardId' =>'身份证号码填写有误'
-        ];
-        $check = $this->validate($data, $rule, $msg);
-        if ($check === true) {
-            //验证通过数据入库
-            $res =  Db::table('user')->insert($data);
-            if($res){
-                return json([
-                    'code'=>0,
-                    'msg'=>'成功'
-                ]);
-            }else{
+        if($request->isPost()){
+            $data = $request->only(['name','mobile','cardId']);
+            //验证数据
+            $rule = [
+                'name' => 'require|length:2,20',
+                'mobile' => 'require|length:11,13',
+                'cardId' => 'required|regex:/^[1-6,8][0-9]{5}[1,2,3][0-9]{3}[0,1][0-9][0-3][0-9][0-9]{3}[0-9,X]$/|unique:address'
+            ];
+            $msg = [
+                'name.require' => '用户名为必填项',
+                'name.length' => '用户名的长度应该在2-20之间',
+                'mobile.length' => '联系电话长度有误',
+                'cardId' =>'身份证号码填写有误'
+            ];
+            $check = $this->validate($data, $rule, $msg);
+            if ($check === true) {
+                //验证通过数据入库
+                $res =  Db::table('user')->insert($data);
+                if($res == true){
+                    return json([
+                        'code'=>0,
+                        'msg'=>'成功'
+                    ]);
+                }else{
+                    return json([
+                        'code'=>1,
+                        'msg'=>'失败'
+                    ]);
+                }
+            }else {
+                //前端输入的数据有误
                 return json([
                     'code'=>1,
-                    'msg'=>'失败'
+                    'msg'=>$check
                 ]);
             }
-        }else {
-            //前端输入的数据有误
-            return json([
-                'code'=>1,
-                'msg'=>''
-            ]);
         }
     }
 }
